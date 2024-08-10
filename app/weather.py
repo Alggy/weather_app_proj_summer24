560f8ca9dd126593594f133d13dce714ea2899b5
Alggy <165315085+Alggy@users.noreply.github.com>
2024-08-10 13:04:06 -0400

    weather app forecast

--- app/weather.py
+++ app/weather.py
@@ -1,21 +1,49 @@
+
+from pgeocode import Nominatim
+import requests
+import json
+from IPython.display import Image, display
+
 degree_sign = u"\N{DEGREE SIGN}"
 
-#import pgeocode
-from pgeocode import Nominatim
-import json
-import requests
+def display_forecast(zip_code, country_code="US"):
+    """
+    Displays a seven day weather forecast for the provided zip code.
 
-nomi = Nominatim('US')
-geo = nomi.query_postal_code("07302")
+    Params :
+
+        country_code (str) a valid country code (see supported country codes list). Default is "US".
+
+        zip_code (str) a valid US zip code, like "20057" or "06510".
+
+    """
+
+    nomi = Nominatim(country_code)
+    geo = nomi.query_postal_code(zip_code)
+    latitude = geo["latitude"]
+    longitude = geo["longitude"]
+
+    request_url = f"https://api.weather.gov/points/{latitude},{longitude}"
+    response = requests.get(request_url)
+    #print(response.status_code)
+    parsed_response = json.loads(response.text)
+
+    forecast_url = parsed_response["properties"]["forecast"]
+    forecast_response = requests.get(forecast_url)
+    #print(forecast_response.status_code)
+    parsed_forecast_response = json.loads(forecast_response.text)
+
+    periods = parsed_forecast_response["properties"]["periods"]
+    daytime_periods = [period for period in periods if period["isDaytime"] == True]
+
+    for period in daytime_periods:
+        #print(period.keys())
+        print("-------------")
+        print(period["name"], period["startTime"][0:7])
+        print(period["shortForecast"], f"{period['temperature']} {degree_sign}{period['temperatureUnit']}")
+        #print(period["detailedForecast"])
+        display(Image(url=period["icon"]))
 
 
-latitude = geo["latitude"]
-longitude = geo["longitude"]
-
-request_url = f"https://api.weather.gov/points/{latitude},{longitude}"
-# print(request_url)
-response = requests.get(request_url)
-parsed_response = json.loads(response.text)
-
-print(response.status_code)
-parsed_response
+my_zip = '07302'
+display_forecast(my_zip)
---
+++ test/weather_test.py
@@ -0,0 +1,4 @@
+# this is the "test/stocks_test.py" file...
+
+def test_example():
+    assert 2 + 2 == 4
