b46bd9ae5b50affcc3d2e1f6ece2ad407a2939ef
Alggy <165315085+Alggy@users.noreply.github.com>
2024-08-10 14:46:56 -0400

    Update __init__.py

---
+++ web_app/__init__.py
@@ -0,0 +1,16 @@
+# this is the "web_app/__init__.py" file...
+
+from flask import Flask # type: ignore
+
+from web_app.routes.weather_routes import home_routes
+from web_app.routes.stocks_routes import stocks_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(home_routes)
+    app.register_blueprint(stocks_routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
