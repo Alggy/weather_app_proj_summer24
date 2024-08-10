da2c32867378cf683bdfbbf941cc5d0da2736fc0
Alggy <165315085+Alggy@users.noreply.github.com>
2024-08-10 14:25:10 -0400

    for loop weather forecast, some inital work on web app and testing

--- app/weather.py
+++ app/weather.py
@@ -44,47 +44,47 @@ def display_forecast(zip_code, country_code="US"):
     periods = parsed_forecast_response["properties"]["periods"]
     daytime_periods = [period for period in periods if period["isDaytime"] == True]
 
-    #for period in daytime_periods:
-    #    #print(period.keys())
-    #    print("-------------")
-    #    print(period["name"], period["startTime"][0:7])
-    #    print(period["shortForecast"], f"{period['temperature']} {degree_sign}{period['temperatureUnit']}")
-        #print(period["detailedForecast"])
-    #    display(Image(url=period["icon"]))
+    for period in daytime_periods:
+        #print(period.keys())
+        print("-------------")
+        print(period["name"], period["startTime"][0:7])
+        print(period["shortForecast"], f"{period['temperature']} {degree_sign}{period['temperatureUnit']}")
+       #print(period["detailedForecast"])
+        display(Image(url=period["icon"]))
 
-    df = DataFrame(daytime_periods)
-    
-    df["date"] = df["startTime"].apply(chopped_date)
+    # df = DataFrame(daytime_periods)
+    # 
+    # df["date"] = df["startTime"].apply(chopped_date)
+# 
+    # # df["img"] = df["icon"].apply(to_image)
+# 
+    # # combined column for temp display
+    # # ... h/t: https://stackoverflow.com/questions/19377969/combine-two-columns-of-text-in-pandas-dataframe
+    # df["temp"] = df["temperature"].astype(str) + " " + degree_sign + df["temperatureUnit"]
+# 
+    # # rename cols:
+    # df.rename(columns={
+    #     "name":"day",
+   #     "shortForecast": "forecast"
+   # }, inplace=True)
 
-    # df["img"] = df["icon"].apply(to_image)
+   # # drop unused cols:
+   # df.drop(columns=[
+   #     "temperature", "temperatureUnit", "temperatureTrend",
+   #     "windSpeed", "windDirection",
+   #     "startTime", "endTime",
+   #     "number", "isDaytime", "detailedForecast"
+   # ], inplace=True)
 
-    # combined column for temp display
-    # ... h/t: https://stackoverflow.com/questions/19377969/combine-two-columns-of-text-in-pandas-dataframe
-    df["temp"] = df["temperature"].astype(str) + " " + degree_sign + df["temperatureUnit"]
-
-    # rename cols:
-    df.rename(columns={
-        "name":"day",
-        "shortForecast": "forecast"
-    }, inplace=True)
-
-    # drop unused cols:
-    df.drop(columns=[
-        "temperature", "temperatureUnit", "temperatureTrend",
-        "windSpeed", "windDirection",
-        "startTime", "endTime",
-        "number", "isDaytime", "detailedForecast"
-    ], inplace=True)
-
-    # re-order columns:
-    df = df.reindex(columns=['day', 'date', 'temp', 'forecast', 'icon'])
-    
-    # return df
-    print("---")
-    print("SEVEN DAY FORECAST")
-    print("LOCATION:", f"{geo.place_name}, {geo.state_code}".upper())
-    print("---")
-    return HTML(df.to_html(escape=False, formatters=dict(icon=to_image)))
+   # # re-order columns:
+   # df = df.reindex(columns=['day', 'date', 'temp', 'forecast', 'icon'])
+   # 
+   # # return df
+   # print("---")
+   # print("SEVEN DAY FORECAST")
+   # print("LOCATION:", f"{geo.place_name}, {geo.state_code}".upper())
+   # print("---")
+   # return HTML(df.to_html(escape=False, formatters=dict(icon=to_image)))
 
 
 
--- test/weather_test.py
+++ test/weather_test.py
@@ -2,3 +2,12 @@
 
 def test_example():
     assert 2 + 2 == 4
+
+from pandas import DataFrame
+from app.weather import display_forecast
+
+def test_data_fetching():
+    df = display_forecast("07302")
+    assert isinstance(df, DataFrame)
+    assert df.columns.tolist() == ['day', 'date', 'temp', 'forecast', 'icon']
+    assert geo.place_name == "Jersey City"
